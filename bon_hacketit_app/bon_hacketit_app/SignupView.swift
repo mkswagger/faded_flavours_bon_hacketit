ef0b6da1841d4fb9e7feac8ac629b59af988414f
flaminshinjan <srikpatra.2008@gmail.com>
2023-04-09 01:38:28 +0530

    added some views

---
+++ bon_hacketit_app/bon_hacketit_app/SignupView.swift
@@ -0,0 +1,20 @@
+//
+//  SignupView.swift
+//  bon_hacketit_app
+//
+//  Created by Shinjan Patra on 09/04/23.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
---
+++ bon_hacketit_app/bon_hacketit_app/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  bon_hacketit_app
+//
+//  Created by Shinjan Patra on 09/04/23.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
