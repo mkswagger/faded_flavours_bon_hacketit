54e7d29d91b18f80a2809778b52d9ab9c987c53d
flaminshinjan <srikpatra.2008@gmail.com>
2023-04-09 01:31:45 +0530

    App Part

---
+++ bon_hacketit_app/bon_hacketit_app/bon_hacketit_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bon_hacketit_appApp.swift
+//  bon_hacketit_app
+//
+//  Created by Shinjan Patra on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct bon_hacketit_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bon_hacketit_app/bon_hacketit_appUITests/bon_hacketit_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bon_hacketit_appUITestsLaunchTests.swift
+//  bon_hacketit_appUITests
+//
+//  Created by Shinjan Patra on 09/04/23.
+//
+
+import XCTest
+
+final class bon_hacketit_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
